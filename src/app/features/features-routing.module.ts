fe14e8db5ebc491f7d1e3e6657ae1622a9b7b630
Jessé Antônio <jesseantonio1321@gmail.com>
2023-02-06 19:46:47 -0300

    deleting useless files

---
+++ src/app/features/features-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PageNotFoundComponent } from "../core/page-not-found/page-not-found.component";
+
+const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'home',
+      pathMatch: 'full',
+    },
+    {
+      path: 'home',
+      loadChildren: async () =>
+        import('./home/home.module').then((m) => m.HomeModule),
+    },
+
+    {
+      path: 'teams',
+      loadChildren: async () =>
+        import('./team/team.module').then((m) => m.TeamModule),
+    },
+
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class FeaturesRoutingModule {}
---
+++ src/app/features/team/table/table.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TeamsService } from 'src/app/core/services/teams.service';
+
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  public logos = []
+  public teams = [];
+  public headers = ['Clube', 'Pontos', 'Partidas Jogadas', 'Vitórias', 'Empates', 'Derrotas', 'Gols Marcados', 'Gols Sofridos', 'Saldo de Gols'];
+  public stats: any;
+  public selectedLeague: any;
+
+  constructor(private teamsService: TeamsService, private router: Router) {
+    this.selectedLeague = this.router.getCurrentNavigation();
+   }
+
+  ngOnInit(): void {
+    this.getTeamStats();
+    this.getTeamLogo();
+  }
+
+  public getTeamStats() {
+    this.teamsService.all(this.selectedLeague.extras.state).subscribe((value: any) => {
+      this.teams = value.data.standings.map(standing =>
+        ({ ...standing.team, ...{ stats: standing.stats.filter(stat => !['All Splits', 'deductions', 'ppg', 'rankChange', 'rank'].includes(stat.name)).sort() } }))
+    })
+  }
+
+  public getTeamLogo() {
+    this.teamsService.logoTime().subscribe((value: any) => {
+      let logosTimes = value.data.map(standing => standing.logos["light"])
+      this.logos = logosTimes
+    })
+  }
+
+  public formatTeamStats(obj: any) {
+    const [jogosJogados, derrotas, pontos, GC, GM, empates, vitorias, saldoDeGols] = obj;
+    const newObjStats = {pontos, jogosJogados, vitorias, empates, derrotas, GM, GC, saldoDeGols}
+    
+    return this.stats = Object.keys(newObjStats).map(function (key) { return newObjStats[key] });
+  }
+}
